4e6afba564bc7fb545348c6e687c8fb347eb811b
Shuviu <shuviu@proton.me>
2025-09-22 23:19:26 +0200

    Implemented API calls from the front- to the backend Might have also added node qwq

---
+++ Frontend/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("./src"))
+
+// 5. Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ Frontend/src/js/apiHandler.js
@@ -0,0 +1,27 @@
+const baseUrl = "http://localhost:8080/"
+
+async function registerNewLongUrl(event) {
+    event.preventDefault()
+    let inputField = event.target.elements.longUrlInput
+    let longUrl = inputField.value
+    inputField.value = ""
+    
+    let shortUrl;
+
+    try {
+        const res = await fetch(baseUrl + "register?url=" + longUrl);
+        if(!res.ok){
+            throw new Error()
+        }
+        shortUrl = await res.text()
+    }catch (error){
+        console.error("An error occured: ", error)
+        return;
+    }
+
+    var shortUrlDisplay = document.getElementById("shortUrlDisplay")
+    if (shortUrlDisplay === undefined){
+        console.error("Could not access the shortUrlDisplay")
+    }
+    shortUrlDisplay.value = baseUrl + shortUrl
+}
