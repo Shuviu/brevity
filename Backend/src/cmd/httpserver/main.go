8820fe078cedbff52917702c4053f0a85e5075b1
Shuviu <shuviu@proton.me>
2025-09-22 23:20:20 +0200

    Updated go api to use proper cors setup.

--- Backend/src/cmd/httpserver/main.go
+++ Backend/src/cmd/httpserver/main.go
@@ -5,14 +5,29 @@ import (
 	"brevity/internal/sqliteDatabase"
 	"fmt"
 	"net/http"
+
+	"github.com/rs/cors"
 )
 
 func main() {
 	router := http.NewServeMux()
+
+	corsHandler := cors.New(cors.Options{
+		// Allow your frontend's origin
+		AllowedOrigins: []string{"http://localhost:3000", "https://brevity.shuviu.de"},
+		// Add other necessary headers and methods
+		AllowedMethods: []string{"GET"},
+		AllowedHeaders: []string{"Content-Type", "Authorization"},
+		// Set to true if your frontend needs to send credentials like cookies
+		AllowCredentials: true,
+	})
+
+	handler := corsHandler.Handler(router)
 	var server = http.Server{
 		Addr:    ":8080",
-		Handler: router,
+		Handler: handler,
 	}
+
 	fmt.Println("Starting Initialization...")
 	sqliteDatabase.InitializeDatabase()
 	httpserver.InitializeRouter(router)
--- Backend/src/go.mod
+++ Backend/src/go.mod
@@ -2,4 +2,7 @@ module brevity
 
 go 1.25
 
-require github.com/mattn/go-sqlite3 v1.14.32
+require (
+	github.com/mattn/go-sqlite3 v1.14.32
+	github.com/rs/cors v1.11.1
+)
